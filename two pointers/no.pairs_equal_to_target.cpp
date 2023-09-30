a72923c60e8638ea27514de2ad41961429321875
ghada <ghadaemad011@gmail.com>
2023-09-30 11:33:08 +0300

    modified file

--- two pointers/no.pairs_equal_to_target.cpp
+++ two pointers/no.pairs_equal_to_target.cpp
@@ -1,34 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int NumberofPairs(int n){
-    return floor(n*(n+1)/2);
+int NumberofPairs(int n)
+{
+    return floor(n * (n + 1) / 2);
 }
-int Sum(vector<int>arr,int target){
-    int start=0;
-    int end=arr.size();
-    int paircnt=0;
-    while(start<end){
-        if(arr[start]+arr[end]>target){
+int Sum(vector<int> arr, int target)
+{
+    int start = 0;
+    int end = arr.size();
+    int paircnt = 0;
+    while (start < end)
+    {
+        if (arr[start] + arr[end] > target)
+        {
             end--;
         }
-        else if(arr[start]+arr[end]< target){
+        else if (arr[start] + arr[end] < target)
+        {
             start++;
         }
-        else{
-            if(arr[start]!=arr[end]){
-                int cnt1=0,cnt2=0;
-                int leftel=arr[start];
-                int rightEl=arr[end];
-                while(arr[start]==leftel){
-                    start++;cnt1++;
+        else
+        {
+            if (arr[start] != arr[end])
+            {
+                int cnt1 = 0, cnt2 = 0;
+                int leftel = arr[start];
+                int rightEl = arr[end];
+                while (arr[start] == leftel)
+                {
+                    start++;
+                    cnt1++;
                 }
-                while(arr[end]==rightEl){
-                    end--;cnt2++;
+                while (arr[end] == rightEl)
+                {
+                    end--;
+                    cnt2++;
                 }
-                paircnt=paircnt+(cnt1*cnt2);
+                paircnt = paircnt + (cnt1 * cnt2);
             }
-            if(arr[start]==arr[end]){
-                paircnt=paircnt+NumberofPairs(end-start);
+            if (arr[start] == arr[end])
+            {
+                paircnt = paircnt + NumberofPairs(end - start);
                 return paircnt;
             }
         }
@@ -36,8 +48,9 @@ int Sum(vector<int>arr,int target){
     return paircnt;
 }
 
-int main(){
-    vector<int>arr={1,4,4,5,5,5,6,6,11};
-   
-   Sum(arr,11);
+int main()
+{
+    vector<int> arr = {1, 4, 4, 5, 5, 5, 6, 6, 11};
+
+    Sum(arr, 11);
 }
